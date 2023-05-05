db32e2871f4c48125a6c519be4192749201d2676
carbo <git@github.com:sakura745/C-.git>
2023-05-05 17:09:45 +0800

    May 5, 2023 - 2

--- chapter2/PtrRef5.cpp
+++ chapter2/PtrRef5.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 
 int main() {
-    int x = 3;
+    int x = 3, y = 4;
     int &ref = x;//reference
+    ref = y;//不表示引用的重新绑定，表示为把y赋值给ref(x)
     std::cout << ref << '\n';
     ref = ref + 1;
     std::cout << x << '\n';
