3b292a0f1aba34f42ad454e9cdc490b03ffbb893
carbo <git@github.com:sakura745/C-.git>
2023-05-08 23:27:07 +0800

    May 8, 2023 - 3

---
+++ chapter6/consteval.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+//只能在编译期
+consteval int fun(int x) {
+    return x + 1;
+}
+
+int main() {
+    constexpr int x = fun(3);
+
+//illegal
+//    int y;
+//    std::cin >> y;
+//    return fun(y);
+}
