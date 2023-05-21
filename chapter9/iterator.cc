4cc37c8c8b065c5fbc2a57726f0c234f36c064f3
carbo <git@github.com:sakura745/C-.git>
2023-05-21 22:47:09 +0800

    May 21, 2023 - 3

---
+++ chapter9/iterator.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+    std::vector<int> a{1, 2, 3};
+    auto b = a.begin();
+    auto e = a.end();
+    for (auto ptr = b; ptr < e; ++ptr) {
+        std::cout << *ptr << ' ';
+    }
+
+    auto f = a.rbegin();//reverse
+    auto g = a.rend();
+    std::cout << std::endl;
+    for (auto ptr = f; ptr < g; ++ptr) {
+        std::cout << *ptr << ' ';
+    }
+
+    std::vector<int> a1{1, 2, 3};
+    auto b1 = a1.cbegin();
+    auto e1 = a1.cend();
+    for (auto ptr = b1; ptr < e1; ++ptr) {
+        //*ptr = 3;//illegal.迭代器只读
+    }
+
+    const std::vector<int> a2{1, 2, 3};
+    auto b2 = a2.begin();
+    auto e2 = a2.end();
+    for (auto ptr = b1; ptr < e1; ++ptr) {
+        //*ptr = 3;//illegal。常量数组，只读
+    }
+
+}
